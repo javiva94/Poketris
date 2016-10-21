41508ddcef472ac3767b58b99535a00e72c37e7d
Javier <javi@macbook-pro-de-javier-3.local>
2016-10-21 14:26:24 +0200

    Initial Commit

---
+++ Poketris-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poketris-1
+//
+//  Created by Javier on 21/10/16.
+//  Copyright © 2016 Javier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
