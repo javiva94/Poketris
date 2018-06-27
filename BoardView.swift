b2255c2c5e5e94b8bc0d0fe2a924ce38c9675708
Javier <javi@MacBook-Pro-de-Javier-3.local>
2018-06-27 02:06:39 +0200

    primer commit

---
+++ BoardView.swift
@@ -0,0 +1,21 @@
+//
+//  BoardView.swift
+//  Poketris-1
+//
+//  Created by Javier on 21/10/16.
+//  Copyright © 2016 Javier. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
