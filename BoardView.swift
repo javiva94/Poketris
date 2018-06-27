89bba7b9a4aa36dab76b93eac32e1c03f096b1c9
Javier <javi@MacBook-Pro-de-Javier-3.local>
2018-06-27 02:06:50 +0200

    Version 1

--- BoardView.swift
+++ BoardView.swift
@@ -8,14 +8,109 @@
 
 import UIKit
 
+protocol BoardViewDataSource: class {
+    func numberOfRows(in: BoardView) -> Int
+    func numberOfColumns(in: BoardView) -> Int
+    func isSolid(_ blockView: BoardView, atRow row: Int, atColumn column: Int) -> Bool
+    func backgroundImageName( in boardView: BoardView, atRow row : Int, atColumn column : Int ) -> UIImage?
+    func foregroundImageName( in boardView: BoardView, atRow row : Int, atColumn column : Int ) -> UIImage?
+    
+}
+
+@IBDesignable
 class BoardView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    
+    weak var dataSource: BoardViewDataSource!
+    
+    // cambiar por un datasource
+    //var board: Board!
+    
+    @IBInspectable
+    var bgColor: UIColor = UIColor.orange
+    
+    
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        updateBoxSize()
+        drawBackground()
+        drawBlocks()
     }
-    */
-
+    
+    
+    private func drawBackground(){
+        
+        let rows = dataSource.numberOfRows(in: self)
+        let columns = dataSource.numberOfColumns(in: self)
+        
+        
+        let canvasX = box2Point(0)
+        let canvasY = box2Point(0)
+        let canvasWidth = box2Point(columns)
+        let canvasHeight = box2Point(rows)
+        
+        
+        let path = UIBezierPath(rect: CGRect(x: canvasX,
+                                             y: canvasY,
+                                             width: canvasWidth,
+                                             height: canvasHeight))
+        bgColor.setFill()
+        path.fill()
+        
+    }
+    
+    private func drawBlocks() {
+        //Tamaño del tablero
+        let rows = dataSource.numberOfRows(in: self)
+        let columns = dataSource.numberOfColumns(in: self)
+        
+        for r in 0..<rows{
+            for c in 0..<columns{
+                drawBox(row: r, column: c)
+                
+            }
+        }
+    }
+    
+    
+    private func drawBox(row : Int, column : Int){
+        let x = box2Point(column)
+        let y = box2Point(row)
+        let width = box2Point(1)
+        let height = box2Point(1)
+        
+        let rect = CGRect(x: x, y: y, width: width, height: height)
+        
+        if let bgImg = dataSource.backgroundImageName(in: self, atRow: row, atColumn: column){
+            
+            bgImg.draw(in: rect)
+        }
+        
+        if let fgImg = dataSource.foregroundImageName(in: self, atRow: row, atColumn: column){
+            
+            fgImg.draw(in: rect)
+        }
+    }
+    private var boxSize: CGFloat = 0
+    
+    private func updateBoxSize(){
+        let rows = dataSource.numberOfRows(in: self)
+        let columns = dataSource.numberOfColumns(in: self)
+        
+        let boxwidth = bounds.size.width / CGFloat(columns)
+        let boxHeight = bounds.size.height / CGFloat(rows)
+        
+        boxSize = min(boxwidth,boxHeight)
+        
+        
+    }
+    
+    private func box2Point(_ box: Int) ->CGFloat{
+        
+        return CGFloat(box)*boxSize
+    }
+    
+    
+    
+    
+    
 }
--- Poketris-1/ViewController.swift
+++ Poketris-1/ViewController.swift
@@ -8,18 +8,245 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, BoardDelegate, BoardViewDataSource{
+    
+    var board: Board!
+    
+    @IBOutlet weak var nextBlockView: BoardView!
+    @IBOutlet weak var boardView: BoardView!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var lineas: UILabel!
+    
+    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
+        board.moveRight()
+        boardView.setNeedsDisplay()
+    }
+    
+    @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+        board.moveLeft()
+        boardView.setNeedsDisplay()
+    }
+    
+    @IBAction func swipeUp(_ sender: UISwipeGestureRecognizer) {
+        board.rotate(toRight: true)
+        boardView.setNeedsDisplay()
+    }
+    
+    @IBAction func swipeDown(_ sender: UISwipeGestureRecognizer) {
+        board.moveDown()
+        boardView.setNeedsDisplay()
+    }
+    
+    @IBAction func tapDown(_ sender: UILongPressGestureRecognizer) {
+        board.dropDown()
+        boardView.setNeedsDisplay()
+    }
+    
+    
+    var contador = 0
+    var timer: Timer?
+    var gameInProgress = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        board = Board()
+        board.delegate = self
+        boardView.dataSource = self
+        nextBlockView.dataSource = self
+        startNewGame()
     }
-
+    
+    
+    func startNewGame(){
+        board.newGame()
+        gameInProgress = true
+        autoMoveDown()
+    }
+    
+    func autoMoveDown(){
+        guard gameInProgress else {return}
+        
+        board.moveDown(insertNewBlockIfNeeded: true)
+        boardView.setNeedsDisplay()
+        nextBlockView.setNeedsDisplay()
+        
+        let interval = 1.0
+        
+        timer = Timer.scheduledTimer(timeInterval: interval,
+                                     target: self,
+                                     selector: #selector(autoMoveDown),
+                                     userInfo: nil,
+                                     repeats: false)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - Controller
+    
+    
+    @IBAction func moveLeft() {
+        board.moveLeft()
+        boardView.setNeedsDisplay()
+    }
+    
+    
+    @IBAction func moveRight() {
+        board.moveRight()
+        boardView.setNeedsDisplay()
+    }
+    
+    
+    @IBAction func rotate() {
+        board.rotate(toRight: true)
+        boardView.setNeedsDisplay()
+    }
+    
+    
+    @IBAction func moveDown() {
+        board.moveDown()
+        boardView.setNeedsDisplay()
+    }
+    
+    // MARK: - Delegado
+    
+    func gameOver() {
+        print("Game Over")
+        gameInProgress = false
+        
+        // crear un UIAlertController:
+        let alert = UIAlertController(title: "GAME OVER",
+                                      message: "Pulsa Ok para empezar",
+                                      preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: . default) {aa in self.startNewGame()})
+        
+        // presentar el Alert Controller
+        present(alert, animated: true, completion: nil)
+        contador = 0
+        score.text = "Score " + String(contador)
+        lineas.text = "Lineas conseguidas" + String(contador)
+    }
+    
+    func rowCompleted() {
+        print("Row Completed!")
+        contador += 100
+        score.text = "Score " + String(contador)
+        lineas.text = "Lineas conseguidas " + String(contador / 100)
+        score.setNeedsDisplay()
+        lineas.setNeedsDisplay()
+        
+        
+        //ponemos un contador que cuando se complete fila aumente en 1 y le ponemos valor con etiqueta
+    }
+    
+    // MARK: - Data Source tablero
+    
+    func numberOfRows(in boardView: BoardView) -> Int {
+        switch boardView {
+        case self.boardView:
+            return board.rowsCount
+        case self.nextBlockView:
+            return board.nextBlock?.height ?? 0
+        default:
+            return 0
+        }
+    }
+    
+    func numberOfColumns(in boardView: BoardView) -> Int {
+        switch boardView{
+        case self.boardView:
+            return board.columnsCount
+        case self.nextBlockView:
+            return board.nextBlock?.width ?? 0
+        default:
+            return 0
+        }
+    }
+    
+    func backgroundImageName(in boardView: BoardView, atRow row: Int, atColumn column: Int) -> UIImage? {
+        switch boardView{
+        case self.boardView:
+            if let texture = board.currentTexture(atRow: row, atColumn: column){
+                let imageName = texture.backgroundImageName()
+                return cachedImage(name: imageName)
+            }
+            else {
+                return nil
+            }
+        case self.nextBlockView:
+            guard let block = board.nextBlock, block.isSolid(row: row, column: column) else {
+                return nil
+            }
+            let imageName = block.texture.backgroundImageName()
+            return cachedImage(name: imageName)
+        default:
+            return nil
+        }
+    }
+    
+    func foregroundImageName(in boardView: BoardView, atRow row: Int, atColumn column: Int) -> UIImage? {
+        switch boardView {
+        case self.boardView:
+            if let texture = board.currentTexture(atRow: row, atColumn: column) {
+                let imageName = texture.pokemonImageName()
+                return cachedImage(name: imageName)
+            } else {
+                return nil
+            }
+        case self.nextBlockView:
+            guard let block = board.nextBlock, block.isSolid(row: row, column: column)
+                else {
+                    return nil
+            }
+            let imageName = block.texture.pokemonImageName()
+            return cachedImage(name: imageName)
+        default:
+            return nil
+        }
+    }
+    
+    /*
+     func blockWidth(for: BoardView) -> Int{
+     guard let block = board.nextBlock else {
+     return false
+     }
+     return block.isSolid(row: row, column: column)
+     }
+     */
+    func isSolid (_ blockView: BoardView, atRow row: Int, atColumn column: Int) -> Bool {
+        guard let block = board.nextBlock else {
+            return false
+        }
+        return block.isSolid(row: row, column: column)
+    }
+    
+    func texture (of: BoardView) -> Texture {
+        guard let block = board.nextBlock else {
+            return -1
+        }
+        return block.texture
+    }
+    
+    var imagesCache = [String: UIImage]()
+    
+    private func cachedImage(name imageName: String) -> UIImage? {
+        if let image = imagesCache[imageName] {
+            return image
+        }
+        else if let image = UIImage(named: imageName) {
+            imagesCache[imageName] = image
+            return image
+        }
+        return nil
+    }
+    
+    
+    
+    func texture(in: BoardView, atRow row: Int, atColumn column: Int) -> Texture? {
+        let texture = board.currentTexture(atRow: row, atColumn: column)
+        return texture
+    }
+    
 }
-
